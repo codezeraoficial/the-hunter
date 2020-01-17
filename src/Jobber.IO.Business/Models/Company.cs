fde656059e9fe48ec22bd51ccb190830ef6a1eec
codezeraoficial <57764517+codezeraoficial@users.noreply.github.com>
2020-01-17 14:25:15 -0300

    Update Company.cs
    Ajustando Company Model

--- src/Jobber.IO.Business/Models/Company.cs
+++ src/Jobber.IO.Business/Models/Company.cs
@@ -10,13 +10,9 @@ namespace Jobber.IO.Business.Models
         public string Email { get; set; }
         public bool Active { get; set; }
         public KindOfCompany KindOfCompany { get; set; }
-        public Address Address { get; set; }
-
-        public Occupation OccupationSeek { get; set; }
-        public KindOccupation KindOfEmployee { get; set; }
+        public Address Address { get; set; }       
+        
         public KindPlan KindPlan { get; set; }
-
-        public IEnumerable<Tech> Techs { get; set; }
-        public IEnumerable<JobOffer> Jobbers { get; set; }
+        
     }
 }
