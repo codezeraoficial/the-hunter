3a11d06ca8e56657b61bd056498dfcfece225f8a
Leonardo Viana <leonardo.viana@meep-app.com>
2020-02-03 21:10:47 -0300

    ##12 Adding DTO's  (View Models)

---
+++ src/Jobber.IO.Server/ViewModels/CompanyViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jobber.IO.Server.ViewModels
+{
+    public class CompanyViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} must have between {2} and {1} characteres", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(14, ErrorMessage = "The field {0} must have between {2} and {1} characteres", MinimumLength = 11)]
+        public string Document { get; set; }
+
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(50, ErrorMessage = "The field {0} must have between {2} and {1} characteres", MinimumLength = 8)]
+        public string Email { get; set; }
+
+        public bool Active { get; set; }
+
+        public AddressViewModel Address { get; set; }
+
+        public int KindPlan { get; set; }
+        public int KindOfCompany { get; set; }
+
+        public IEnumerable<JobOfferViewModel> JobOffers { get; set; }
+    }
+}
---
+++ src/Jobber.IO.Server/ViewModels/TechViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jobber.IO.Server.ViewModels
+{
+    public class TechViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(50, ErrorMessage = "The field {0} must have between {2} and {1} characteres", MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [StringLength(1000, ErrorMessage = "The field {0} must have between {2} and {1} characteres", MinimumLength = 10)]
+        public string Description { get; set; }
+
+        public int Level { get; set; }
+
+        public Guid EmployeeId { get; set; }    
+
+        public Guid JobOfferId { get; set; }
+        
+    }
+}
